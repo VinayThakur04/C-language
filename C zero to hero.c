0deb20ca8bdfd17f8ae5543410667f5c951cab16
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-01-26 12:42:35 +0530

    init

---
+++ C zero to hero.c
@@ -0,0 +1,47 @@
+//WAP to print your name without using variable!
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("Vinay Kumar Thakur");
+	return 0;
+}
+//WAP print your biodata like name , registration number , course name , city , state , contact number without using variable!
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("Vinay Kumar Thakur\n");
+	printf("00112233\n");
+	printf("CSE 101\n");
+	printf("Darbhanga\n");
+	printf("Bihar\n");
+	printf("9999999999\n");
+	return 0;
+}
+// Wap write a program to print the value of int. , float , char. , and double data type variable!
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a=5;
+	float b=5.14;
+	char c='A';
+	double d=15.110811;
+	printf("%d" , a);
+	printf("\n%f" , b);
+	printf("\n%c" , c);
+	printf("\n%lf" , d);
+	return 0;
+}
+//WAP write a program a to calculate the addition of two number!
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a = 5;
+	int b = 7;
+	int sum = a + b;
+	printf("%d", sum);
+	return 0;
+}
