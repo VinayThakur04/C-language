ab1a6f0b7efad6714c721e3e1af0754dc8af16ab
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-01-30 22:47:12 +0530

    name change

--- C zero to hero.c
+++ C zero to hero.c
@@ -172,3 +172,39 @@ int main(){
 
     return 0;
 }
+//Given a positive integer denoting , do the following:
+//If 1 <=n<=9, print the lowercase English word corresponding to the number (e.g., one for 1, two for 2, etc.).
+//Ifn>9 , print Greater than 9. in c programming.
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    if (n == 1)
+        printf("one");
+    else if (n == 2)
+        printf("two");
+    else if (n == 3)
+        printf("three");
+    else if (n == 4)
+        printf("four");
+    else if (n == 5)
+        printf("five");
+    else if (n == 6)
+        printf("six");
+    else if (n == 7)
+        printf("seven");
+    else if (n == 8)
+        printf("eight");
+    else if (n == 9)
+        printf("nine");
+    else
+        printf("Greater than 9");
+
+    return 0;
+}
