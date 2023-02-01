9b72004e7055a101a4d38b16ffe534b8c8f8dfca
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-02-01 19:54:34 +0530

    name change

--- C zero to hero.c
+++ C zero to hero.c
@@ -185,7 +185,24 @@ int main()
 	h=a!=b;
 	printf("In (a!=b) operation result = %d",h);
 	return 0;
-
+}
+//Use of Bitwise Operator!
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("Enter the value of a ");
+	scanf("%d",&a);
+	printf("Enter the value of b ");
+	scanf("%d",&b);
+	printf("a = %d, b=%d\n",a,b);
+	printf("a&b=%d\n",a&b);
+	printf("a|b=%d\n",a|b);
+	printf("a^b=%d\n",a^b);
+	printf("~a=%d\n",a = ~a);
+	printf("b<<1=%d\n",b<<1);
+	printf("b>>1=%d\n",b>>1);
+	return 0;
 }
 // WAP write a programe to print Hello, World! in first line and in second line print the sentence that is input from user!
 #include<stdio.h>
