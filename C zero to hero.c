e7e32892df730b980346a902fed139df7e283b2d
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-02-04 21:03:31 +0530

    name change

--- C zero to hero.c
+++ C zero to hero.c
@@ -375,3 +375,24 @@ int main()
     }
     return 0;
 }
+/*
+* Converts distances from miles to kilometers.
+*/
+
+#include<stdio.h>
+#define KMS_PER_MILE 1.609
+int main(void)
+{
+	double miles,kms; //distance in miles
+	/* Get the distances in miles. */
+	printf("Enter the distances in miles > ");
+	scanf("%lf", &miles);
+	
+	/* Converts the distance to kilometers. */
+	kms = KMS_PER_MILE * miles;
+	
+	/* Display the distance in kilometer. */
+	printf("That equals %f kilometers.\n",kms);
+	
+	return 0;
+}
