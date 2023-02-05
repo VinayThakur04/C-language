4040d782fb9b7290a86dbb1ba55b9008f472ad9c
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-02-05 13:06:55 +0530

    name change

--- C zero to hero.c
+++ C zero to hero.c
@@ -396,3 +396,42 @@ int main(void)
 	
 	return 0;
 }
+//WAP to find largest number!
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n1,n2,n3;
+	printf("Enter n1 value ");
+	scanf("%d",&n1);
+	printf("Enter n2 value ");
+	scanf("%d",&n2);
+	printf("Enter n3 value ");
+	scanf("%d",&n3);
+	
+	// If all numbers are different!
+	if(n1>n2 & n1>n3){
+		printf("%d",n1);
+	}
+	else if(n2>n3 & n2>n1){
+		printf("%d",n2);
+	}
+	else if(n3>n1 & n3>n2){
+		printf("%d",n3);
+	}
+	// If two numbers are same!
+	else if(n1 == n2 & n1 > n3){
+		printf("Two numbers are same which is %d",n1);
+	}
+	else if(n1 == n3 & n1 > n2){
+		printf("Two numbers are same which is %d",n1);
+	}
+	else if(n2 == n3 & n2 > n1){
+		printf("Two numbers are same which is %d",n2);
+	}
+	// If all numbers are same!
+	else
+		printf("All numbers are same");
+
+	return 0;
+}
