7e142096c31298b3131feaae2b2877703729e6f9
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-02-06 21:01:35 +0530

    name change

--- C zero to hero.c
+++ C zero to hero.c
@@ -435,3 +435,20 @@ int main()
 
 	return 0;
 }
+//WAP to calculate the square of those numbers only whose least significant digit is 5.
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+	int s,d;
+	printf("\n Enter a Number :");
+	scanf("%d",&s);
+	d = s%10;
+	if(d==5)
+	{
+		s=s/10;
+		printf("\n Square = %d%d",s*s++,d*d);
+	}
+	else
+		printf("\n Invalid Number");
+}
