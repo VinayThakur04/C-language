9cf1c9ef28159491ed2b116f3de2859cdf26ed3c
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-02-10 19:52:51 +0530

    name change

--- C zero to hero.c
+++ C zero to hero.c
@@ -572,4 +572,18 @@ int main()
 	}
 return 0;
 }
-//Vinay Kumar Thakur
+// Loop Started
+
+//WAP to print numbers upto n given by user.
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the value of n: ");
+	scanf("%d",&n);
+	while (n>0){
+		printf("%d\n",n);
+		n--;
+	}
+	return 0;
+}
