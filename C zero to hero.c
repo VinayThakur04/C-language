2d9af9acefdd984812d6e374b7c45bd1f19fdcf7
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-02-10 19:56:32 +0530

    name change

--- C zero to hero.c
+++ C zero to hero.c
@@ -600,3 +600,16 @@ int main()
 	}
 	return 0;
 }
+//WAP a program to print the table entered by the user using while loop
+#include<stdio.h>
+int main()
+{
+	int a=1,n;
+	printf("Enter the value of n: ");
+	scanf("%d",&n);
+	while (a<=10){
+		printf("%d * %d = %d\n",n,a,a*n);
+		a++;
+	}
+	return 0;
+}
