54e69ce82362fa2b87d653245c9502d002790d0a
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-02-15 01:30:27 +0530

    name change

--- C zero to hero.c
+++ C zero to hero.c
@@ -727,3 +727,24 @@ int main()
 	printf("%d",pro);
 	return 0;
 }
+//WAP to find the photo is accepted or not for n times!
+#include <stdio.h>
+
+int main() {
+    int L, N, W, H;
+    scanf("%d", &L); // read L from input required size
+    scanf("%d",&N);  //N from input numbers of photo
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d %d", &W, &H); // read the width and height of the photo from input
+        if (W < L || H < L) {
+            printf("UPLOAD ANOTHER\n");
+        } else if (W == H) {
+            printf("ACCEPTED\n");
+        } else {
+            printf("CROP IT\n");
+        }
+    }
+
+    return 0;
+}
