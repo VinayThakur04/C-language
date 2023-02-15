d21c993c7c47451b0fb36d09faf766cfdad03cdd
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-02-15 20:50:15 +0530

    name change

--- C zero to hero.c
+++ C zero to hero.c
@@ -771,3 +771,43 @@ int main(){
 		}
 	return 0;
 }
+//WAP to perform the addidion of two matrix
+#include<stdio.h>
+int main()
+{
+	int r,c,i,j;
+	printf("Enter a value for rows and columns you want: ");
+	scanf("%d %d",&r,&c);
+	int a[r][c];
+	printf("Enter a value for 1st matrix:\n");
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+	/*Second matrix input code*/
+	int b[r][c];
+	printf("Enter a value for 2nd matrix:\n");
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			scanf("%d",&b[i][j]);
+		}
+	}
+	/*Addition of two matrix*/
+	int sum[r][c];
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			sum[i][j]=a[i][j]+b[i][j];
+		}
+	}
+	printf("Final matrix after addition:\n");
+	/*printing of final matrix*/
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			printf("%d\t",sum[i][j]);
+		}
+		/*for printing in matrix form we use \n for nxt line printing*/
+		printf("\n");
+	}
+	return 0;
+}
