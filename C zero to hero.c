7f267f6ccf69416bcadc91537cdcc9c4a7571162
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-02-15 22:09:45 +0530

    name change

--- C zero to hero.c
+++ C zero to hero.c
@@ -851,3 +851,47 @@ int main()
 	}
 	return 0;
 }
+//WAP to perform the multiplication of two matrix
+#include<stdio.h>
+int main()
+{
+	int r,c,i,j,k;
+	printf("Enter a value for rows and columns you want: ");
+	scanf("%d %d",&r,&c);
+	int a[r][c];
+	printf("Enter a value for 1st matrix:\n");
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			scanf("%d",&a[i][j]);
+		}
+	}
+	/*Second matrix input code*/
+	int b[r][c];
+	printf("Enter a value for 2nd matrix:\n");
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			scanf("%d",&b[i][j]);
+		}
+	}
+	/*Multiplication of two matrix*/
+	int mul[r][c];
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			mul[i][j]=0;    
+			for(k=0;k<c;k++)    
+			{    
+			mul[i][j]+=a[i][k]*b[k][j];    
+			}
+		}
+	}
+	printf("Final matrix after multiplication:\n");
+	/*printing of final matrix*/
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			printf("%d\t",mul[i][j]);
+		}
+		/*for printing in matrix form we use \n for nxt line printing*/
+		printf("\n");
+	}
+	return 0;
+}
