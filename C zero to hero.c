38b8b61dc0ff6b5025b1cbfd6ab355fee9168ce9
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-03-22 23:34:30 +0530

    name change

--- C zero to hero.c
+++ C zero to hero.c
@@ -1449,4 +1449,29 @@ int main() {
     printf("%d", ans); 
     return 0;
 }
+//find max and min numbers
+#include<stdio.h>
+int main(){
+	int a[10],n,i,max,min;
+	printf("Enter number of elements: \n");
+	scanf("%d",&n);
+	printf("Enter array elements: \n");
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	min = a[0];
+	for(i=1;i<n;i++){
+		if(a[i]<min){
+		min = a[i];}
+	}
+	
+	max = a[0];
+	for(i=1;i<n;i++){
+		if(a[i]>max){
+		max = a[i];
+		}
+}
+	printf("Min:%d , Max:%d",min,max);
+	return 0;
+}
 //Vinay Kumar Thakur
