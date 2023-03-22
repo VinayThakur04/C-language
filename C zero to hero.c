f768e2c56cc1de9b40f9c86a78627182d08488ec
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-03-22 23:35:55 +0530

    name change

--- C zero to hero.c
+++ C zero to hero.c
@@ -1449,6 +1449,19 @@ int main() {
     printf("%d", ans); 
     return 0;
 }
+//printing base address of an array
+#include<stdio.h>
+int main(){
+	int a[5]={1,2,3,4,5};
+	int i;
+	printf("\n Printing base address of the array:");
+	printf("\n%u %u %u",&a[0],a,&a);
+	printf("\n Printing addresses of all array elements:");
+	for(i=0;i<5;i++){
+		printf("\n%u",&a[i]);
+	}
+	return 0;
+}
 //find max and min numbers
 #include<stdio.h>
 int main(){
