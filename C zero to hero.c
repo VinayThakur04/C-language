5bbf27fa3115c973f6f826e44c7ace0c908e2cee
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-03-25 18:46:11 +0530

    name change

--- C zero to hero.c
+++ C zero to hero.c
@@ -1574,4 +1574,25 @@ int main(){
 	}
 	return 0;
 }
+//WAP to find to delete an element from a given position in 1D array
+#include<stdio.h>
+int main(){
+	int array[100],position,c,n,value;
+	printf("Enter number of elements in array:\n");
+	scanf("%d",&n);
+	printf("Enter %d elements:\n",n);
+	for(c=0;c<n;c++){
+		scanf("%d",&array[c]);
+	}
+	printf("Enter the location where you wish to delete from an array:\n");
+	scanf("%d",&position);
+	for(c=position-1;c<n-1;c++){
+		array[c]=array[c+1];
+	}
+	printf("Resultant array is:\n");
+	for(c=0;c<n-1;c++){
+		printf("%d\n",array[c]);
+	}
+	return 0;
+}
 //Vinay Kumar Thakur
