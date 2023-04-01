fe228dc90cb0b8d91f41a06c2a7514172c9b7efc
Vinay Kumar Thakur <vinaykumar.thakur@lpu.in>
2023-04-01 21:08:03 +0530

    name change

--- C zero to hero.c
+++ C zero to hero.c
@@ -2011,3 +2011,25 @@ int main(){
 	}
 	return 0;
 }
+//Given an array, of size n, reverse it. Example: If array,arr=[1, 2, 3, 4, 5], after reversing it, the array
+// should be, arr = [5, 4, 3, 2, 11]. write c programming code.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+    
+
+
+    /* Write the logic to reverse the array. */
+
+   for (int i = num - 1; i >= 0; i--)
+        printf("%d ", *(arr + i));
+    return 0;
+}
